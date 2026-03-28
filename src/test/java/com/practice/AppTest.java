ecc2a64c08976c37c3b20c5e0c9387168162b69e
Sreehari <srihari.valeti@gmail.com>
2026-03-28 17:59:08 +0530

    test

---
+++ src/test/java/com/practice/AppTest.java
@@ -0,0 +1,12 @@
+package com.practice;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    void testSum() {
+        assertEquals(4, 2 + 2);
+    }
+}
